4525f0845f20e656e9e36f1f6eee0f3de6a29003
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-01 13:10:51 +0530

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        std::set<int, std::greater<int>> nums;
+        std::unordered_map<int, int> freqs;
+
+        for (int num : candidates) {
+            nums.insert(num);
+            freqs.try_emplace(num, 0);
+            freqs[num]++;
+        }
+
+        std::vector<int> numsVec(nums.begin(), nums.end());
+        std::vector<int> sub;
+        int sum = 0;
+
+        std::vector<std::vector<int>> out;
+
+        std::function<void(int)> backtrack = [&](int i) -> void {
+            if (sum == target)
+                out.push_back(sub);
+            if (i > numsVec.size() || sum >= target)
+                return;
+
+            int num = numsVec[i];
+            if (freqs[num] > 0) {
+                sub.push_back(num);
+                sum += num;
+                freqs[num]--;
+
+                backtrack(i);
+
+                sub.pop_back();
+                sum -= num;
+                freqs[num]++;
+            }
+
+            backtrack(i+1);
+        };
+
+        backtrack(0);
+        return out;
+    }
+};

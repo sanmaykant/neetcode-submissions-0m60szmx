fdd265ebeb4dd345e64887a4bdcb400a36131bef
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-01 12:46:44 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> out = new LinkedList<>();
+
+        List<Integer> sub = new LinkedList<Integer>();
+
+        class Helper {
+            int sum = 0;
+
+            void backtrack(int i) {
+                if (sum == target) {
+                    out.add(new LinkedList<Integer>(sub));
+                }
+                if (i >= nums.length || sum >= target) {
+                    return;
+                }
+
+                sub.add(nums[i]);
+                sum += nums[i];
+                backtrack(i);
+
+                sub.remove(sub.size() - 1);
+                sum -= nums[i];
+                backtrack(i+1);
+            }
+        };
+
+        new Helper().backtrack(0);
+        return out;
+    }
+}

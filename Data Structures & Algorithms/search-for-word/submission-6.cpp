19ebe3c2f92fb165dea20d03c3ae88a9e039439c
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-06 08:03:56 +0530

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        std::vector<std::vector<bool>> visited(
+            board.size(), std::vector<bool>(board[0].size(), false));
+        bool found = false;
+
+        std::string s = "";
+        std::function<void(int i, int j)> backtrack = [&](int i, int j) -> void {
+            if (visited[i][j] || found || word[s.size()] != board[i][j])
+                return;
+
+            s.push_back(board[i][j]);
+            visited[i][j] = true;
+
+            if (s.size() == word.size()) {
+                found = true;
+                return;
+            }
+
+            std::vector<std::pair<int, int>> validNeighbours = neighbours(board, i, j);
+            for (auto& n : validNeighbours)
+                backtrack(n.first, n.second);
+
+            s.pop_back();
+            visited[i][j] = false;
+        };
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++)
+                if (!found && board[i][j] == word[0])
+                    backtrack(i, j);
+        }
+
+        return found;
+    }
+
+private:
+    std::vector<std::pair<int, int>> neighbours(
+            std::vector<std::vector<char>>& board, int i, int j) {
+        std::array<std::pair<int, int>, 4> candidates = {
+            std::pair<int, int>{ i, j - 1 },
+            std::pair<int, int>{ i - 1, j },
+            std::pair<int, int>{ i, j + 1 },
+            std::pair<int, int>{ i + 1, j }
+        };
+
+        std::vector<std::pair<int, int>> validNeighbours;
+        for (auto& c : candidates) {
+            if (c.first >= 0 &&
+                c.first < board.size() &&
+                c.second >= 0 &&
+                c.second < board[0].size())
+                    validNeighbours.push_back(c);
+        }
+        return validNeighbours;
+    }
+};

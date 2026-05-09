cd2fe45b9cc2ea39a90edc2dc05cf65ba8e06e13
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-09 12:00:53 +0530

    Add: palindrome-partitioning - submission-14

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-14.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        this.s = s;
+        backtrack(0);
+        return solution;
+    }
+
+    private void backtrack(int b) {
+        if (b == s.length()) {
+            solution.add(new ArrayList(candidate));
+            return;
+        }
+
+        for (int e = b + 1; e <= (int) s.length(); e++) {
+            String substr = s.substring(b, e);
+            if (!isPalindrome(substr))
+                continue;
+            candidate.add(substr);
+            backtrack(e);
+            candidate.remove(candidate.size() - 1);
+        }
+    }
+
+    private boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private String s;
+    private List<String> candidate = new ArrayList<>();
+    private List<List<String>> solution = new ArrayList<>();
+}

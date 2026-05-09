9edd590fb34c7e5744f5ecd3328cc29948140986
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-09 11:23:20 +0530

    Add: palindrome-partitioning - submission-6

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-6.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        std::vector<std::vector<std::string>> out;
+        std::vector<std::string> candidate;
+
+        std::function<void(int)> backtrack = [&](int b) -> void {
+            if (b == s.size()) {
+                out.push_back(candidate);
+                return;
+            }
+
+            for (int l = 1; l <= s.size() - b; l++) {
+                std::string substr = s.substr(b, l);
+                if (!isPalindrome(substr))
+                    continue;
+                candidate.push_back(substr);
+                backtrack(b + l);
+                candidate.pop_back();
+            }
+        };
+        backtrack(0);
+
+        return out;
+    }
+
+private:
+    bool isPalindrome(std::string_view s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};

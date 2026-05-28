66de19bd96799ae1e3a70614f0a41400fb84e9f1
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-05-28 10:36:59 +0530

    Add: find-median-in-a-data-stream - submission-3

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-3.cpp
@@ -0,0 +1,26 @@
+class MedianFinder {
+public:
+    MedianFinder() = default;
+
+    void addNum(int num) {
+        m_MaxHeap.push(num);
+        m_MinHeap.push(m_MaxHeap.top());
+        m_MaxHeap.pop();
+
+        if (m_MaxHeap.size() < m_MinHeap.size()) {
+            m_MaxHeap.push(m_MinHeap.top());
+            m_MinHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (m_MaxHeap.size() == m_MinHeap.size())
+            return static_cast<double>(m_MaxHeap.top() + m_MinHeap.top()) / 2.;
+        else
+            return static_cast<double>(m_MaxHeap.top());
+    }
+
+private:
+    std::priority_queue<int, std::vector<int>, std::less<int>>    m_MaxHeap;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> m_MinHeap;
+};

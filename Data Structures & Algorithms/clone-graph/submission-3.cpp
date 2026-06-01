1f818f53277b705b684a43f2884748fad9fe8109
Sanmay Kant <162121980+sanmaykant@users.noreply.github.com>
2026-06-01 14:14:03 +0530

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return nullptr;
+        return dfs(node);
+    }
+
+private:
+    Node* dfs(Node* node) {
+        auto it = nodes.find(node->val);
+        if (it != nodes.end())
+            return it->second;
+
+        Node* nodeCopy = new Node(node->val);
+        nodes[nodeCopy->val] = nodeCopy;
+
+        for (Node* n : node->neighbors)
+            nodeCopy->neighbors.push_back(dfs(n));
+
+        return nodeCopy;
+    }
+
+private:
+    std::unordered_map<int, Node*> nodes; 
+};
